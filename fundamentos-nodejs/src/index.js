e47220451ba5e7594ecbc294317f3764f84be662
Rodrigo Ferreira <rodrigoferreira2799@gmail.com>
2022-01-22 22:03:07 -0400

    add nodemon

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello worl" });
+  return response.json({ message: "Hello world" });
 });
 
 app.listen(3333);

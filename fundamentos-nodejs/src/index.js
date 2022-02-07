f7e76fdd27a79d6b08ac1d3f34fa33231968477c
Rodrigo Ferreira <rodrigoferreira2799@gmail.com>
2022-02-07 18:33:05 -0400

    add middleware

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,6 +3,7 @@ const { request } = require("express");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/courses", (request, response) => {
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);

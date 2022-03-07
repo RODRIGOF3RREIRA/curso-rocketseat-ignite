3636b03ecb06d2462e87a0ff7f9ff168ffeb3cf4
Rodrigo Ferreira <rodrigoferreira2799@gmail.com>
2022-03-06 21:43:41 -0400

    add motod get

--- Desafio01_Conceitos_do_NodeJs/src/index.js
+++ Desafio01_Conceitos_do_NodeJs/src/index.js
@@ -11,9 +11,9 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { cpf } = request.headers;
+  const { username } = request.headers;
 
-  const user = users.find((user) => user.cpf === cpf);
+  const user = users.find((user) => user.username === username);
 
   if (!user) {
     return response.status(400).json({ error: "User not found" });
@@ -26,22 +26,40 @@ function checksExistsUserAccount(request, response, next) {
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
-  users.push({
+  const userAlreadyExists = users.find((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "User name already exists!" });
+  }
+
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: [],
-  });
-  return response.status(201).send();
+  };
+  users.push(user);
+  return response.status(201).json(user);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  todos.push({
+    id: uuidv4(),
+    title,
+    done,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  });
+  user.todos.push();
+  return response.status(201).send();
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+const {user} = request;
+return response.json(user)
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui

8c4e2505b638fc4e65e8134c916ee6688e4efafb
Rodrigo Ferreira <rodrigoferreira2799@gmail.com>
2022-03-17 17:45:15 -0400

    add inspect for debug

--- Criando_API/src/server.ts
+++ Criando_API/src/server.ts
@@ -2,8 +2,15 @@ import express, { response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World" });
+});
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
 });
 
 app.listen(3333, () => console.log("server is running"));

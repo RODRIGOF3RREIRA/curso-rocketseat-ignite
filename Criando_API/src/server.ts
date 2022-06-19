8055287044892be10dc32e6478b90bfad23bd766
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-06-19 18:43:24 -0400

    chore: configured docker compose

--- Criando_API/src/server.ts
+++ Criando_API/src/server.ts
@@ -11,4 +11,4 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3000, () => console.log('server is running'));
+app.listen(3333, () => console.log('server is running'));
